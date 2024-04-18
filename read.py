c597aaded7f56617ee4225f6b756391cc553d816
mkhemani <114173735+mkhemani@users.noreply.github.com>
2024-04-17 22:22:36 -0400

    Update read.py

--- read.py
+++ read.py
@@ -1,36 +1,7 @@
 # /# Author: CWM
+# #  Modified by: Muskaan Khemani 4/23
 # # Purpose: downloading tables from PDFs
-# # refs:
-# #   https://www.thepythoncode.com/article/extract-pdf-tables-in-python-camelot
-# #   https://tabula-py.readthedocs.io/en/latest/tabula.html
-# # https://github.com/tabulapdf/tabula-java/issues/255
-# # pip install pdfplumber
-# import camelot
-
-# file = "C:\\Users\\cmilando\\OneDrive - Boston University\\Documents\\51 - Optum\\02_Blackouts\\SOR\\13-SOR-Q3\\13-SOR-Q3-Filing-5625.pdf"
-
-# tables = camelot.read_pdf(file, pages="1-20")
-# # hmm does work now
-# tables[0]
-# print(tables[0].df)
-
-
-# # ----------------------------------------------------------------------------
-# import tabula
-# import os
-
-# tables = tabula.read_pdf(file, pages=2)
-
-# tabula.convert_into(file, "output6a.tsv", output_format="tsv", pages="6",
-#                     area = [17, 0, 90, 100],
-#                     relative_area=True,
-#                     #          Town    dt    ID
-#                     columns=[0,    14,    20,   23, 100],
-#                     relative_columns=True)
-
-# -----------------------------------------------------------------------------
-# this is the solution
-# https://github.com/jsvine/pdfplumber/tree/stable#extracting-tables
+# ref: https://github.com/jsvine/pdfplumber/tree/stable#extracting-tables
 # if you don't see (env) run the line `.\env\Scripts\activate`
 import pdfplumber
 import pandas as pd
@@ -96,19 +67,6 @@ with open(file_path, 'w', newline='') as csv_file:
                         #replacing spaces and commas
                         #row[5:8] = [val.replace(' ','').replace(',','')for val in row[5:8]]                
                         csv_writer.writerow(row)
-
-#OUTPUT ERRORS
-#13-SOR-Q3-Filing-5625SectionA: IT IS NOT RECOGNIZING ROW 2, COLUMN 4's third VALUE. Manual check for that needs to be done
-                                # column headers are not included, so have to reference from the OG pdf
-#13-SOR-Q3-Filing-5625SectionB: No need for replacing ',' and ' '. 
-#13-SOR-Q3-Filing-5705: no need to replace spaces or commas. CSV is interesting format because of the pdf, so have to reference the 
-                                # OG pdf to manually check the service territory for each table. 
-#13-SOR-Q4-Filing-8412: similar as above
-
-# NOTE: python arrays/indices go from 0:x, not 1:x
-# NOTE: many similar functions as in R are written in the `pandas` library, so look at those
-# NOTE: unfortunately, this will have to be modified for each pdf, the final workflow will be 
-#       opening this script, changing the filename, changing the page numbers, changeing the vertical lines, and then running
 
 #CODE APPENDIX
 #13-SOR-Q3-Filing-5625SectionA: "explicit_vertical_lines": [35, 114, 154, 186, 226, 350, 385, 425, 456.5, 602, 750]
